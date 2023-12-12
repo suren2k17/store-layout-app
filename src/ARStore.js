2150353d94dbb7c3aee28bebf4c6ea0c5eeb589a
SRAMA49 <Surendar.Ramamoorthy@albertsons.com>
2023-12-11 23:58:57 -0600

    Added changes to ARStore JS

--- src/ARStore.js
+++ src/ARStore.js
@@ -1,12 +1,15 @@
 // src/ARStore.js
 import React from 'react';
+import clusteringImage from './images/ARimage.png'; 
 
 function ARStore() {
   return (
     <div>
-      <h1>AR Store</h1>
-      <p>This is the AR Store page.</p>
-      {/* Additional functionality and content will go here */}
+      <h1>Let's PHYGITALIZE our stores!!</h1>
+      <p>Transform 3D models to AR/VR experience using Unity or Infinity engines</p>
+      <div>
+          <img max-width="100" height="auto" src={clusteringImage} alt="Aisle Clustering Plot" />
+        </div>
     </div>
   );
 }
